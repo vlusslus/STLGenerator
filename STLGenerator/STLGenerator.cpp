61ffdc3b6aa2cc133f387792a054491a90f780aa
vlusslus <konstantinovr1@gmail.com>
2016-08-22 17:21:39 +0300

    Add project files.

---
+++ STLGenerator/STLGenerator.cpp
@@ -0,0 +1,15 @@
+// STLGenerator.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "iostream"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello world!";
+	cin.get();
+    return 0;
+}
+
